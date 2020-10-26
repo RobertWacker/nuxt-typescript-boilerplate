19b281353dc40e3e34090e02bd4bbea83a362163
Yuriy <yuriy.vaker@maximumtest.ru>
2020-10-26 17:47:02 +0300

    Add an error handlear page

---
+++ layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-else>Error {{ error.statusCode }}</h1>
+    <nuxt-link to="/">Back home</nuxt-link>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Vue, Prop } from 'nuxt-property-decorator';
+  import { NuxtError } from '@nuxt/types';
+
+  @Component
+  export default class extends Vue {
+    @Prop() error!: NuxtError;
+  }
+</script>
