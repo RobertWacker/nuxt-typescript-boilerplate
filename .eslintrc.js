44067a6dbb7862881b2118236410d64b26bb6f0c
Yuriy <yuriy.vaker@maximumtest.ru>
2020-10-27 17:19:41 +0300

    Add Eslint settings

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+export default {
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+    },
+    parserOptions: {
+        parser: "babel-eslint",
+    },
+    extends: [
+        "plugin:vue/recommended",
+        "eslint:recommended",
+        "prettier/vue",
+        "plugin:prettier/recommended",
+    ],
+    // required to lint *.vue files
+    plugins: ["vue"],
+    // add your custom rules here
+    rules: {
+        semi: [2, "never"],
+        "no-console": "off",
+        "vue/max-attributes-per-line": "off",
+        "prettier/prettier": ["error", { semi: false }],
+    },
+}
